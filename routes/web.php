77f8fe0f4c91e53d0e28487561d97af2ed8a0e73
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-01-22 17:15:23 -0300

    Ajusta configurações Laravel.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+Route::redirect('/', '/login');
+Route::redirect('/dashboard', '/admin/dashboard');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes(['register' => false]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'prefix'     => 'admin',
+    'as'         => 'admin.',
+    'namespace'  => 'Admin',
+    'middleware' => ['auth'],
+], function () {
+    Route::view('/{any?}', 'layouts.admin.app')->name('dashboard')->where('any', '.*');
 });
