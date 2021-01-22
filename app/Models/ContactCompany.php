2f9cb93b5f0c7fe18a13e23f1f2f72197bd1b60d
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-01-22 18:56:47 -0300

    Adiciona models.

---
+++ app/Models/ContactCompany.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use App\Support\HasAdvancedFilter;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use \DateTimeInterface;
+
+class ContactCompany extends Model
+{
+    use HasAdvancedFilter, SoftDeletes;
+
+    public $table = 'contact_companies';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $orderable = [
+        'id',
+        'company_name',
+        'company_address',
+        'company_website',
+        'company_email',
+    ];
+
+    protected $filterable = [
+        'id',
+        'company_name',
+        'company_address',
+        'company_website',
+        'company_email',
+    ];
+
+    protected $fillable = [
+        'company_name',
+        'company_address',
+        'company_website',
+        'company_email',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
