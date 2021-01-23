197e12f2be313154979fdaa0ba7df1079a2d480a
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-01-22 22:39:46 -0300

    Adiciona plugins.

---
+++ resources/adminapp/js/components/LanguagesPlugin/TopNavbarLanguages.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="nav-item dropdown">
+  </li>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapGetters('I18NStore', ['locale', 'languages'])
+  },
+  methods: {
+    ...mapActions('I18NStore', ['setLocale'])
+  }
+}
+</script>
+
+<style scoped>
+.fixed-width {
+  width: 1.3em;
+  text-align: center;
+  display: inline-block;
+}
+</style>
