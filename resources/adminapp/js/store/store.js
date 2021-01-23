3e67515c8f336b2f8a3e8f0d540164289d580b77
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-01-22 22:48:46 -0300

    Adiciona arquivos de rotas e stores Vue; Inclui módulo de alerta e intl.

---
+++ resources/adminapp/js/store/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import Alert from './modules/alert'
+import I18NStore from './modules/i18n'
+
+import PermissionsIndex from './cruds/Permissions'
+import PermissionsSingle from './cruds/Permissions/single'
+import RolesIndex from './cruds/Roles'
+import RolesSingle from './cruds/Roles/single'
+import UsersIndex from './cruds/Users'
+import UsersSingle from './cruds/Users/single'
+import ContactCompaniesIndex from './cruds/ContactCompanies'
+import ContactCompaniesSingle from './cruds/ContactCompanies/single'
+import ContactContactsIndex from './cruds/ContactContacts'
+import ContactContactsSingle from './cruds/ContactContacts/single'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  modules: {
+    Alert,
+    I18NStore,
+    PermissionsIndex,
+    PermissionsSingle,
+    RolesIndex,
+    RolesSingle,
+    UsersIndex,
+    UsersSingle,
+    ContactCompaniesIndex,
+    ContactCompaniesSingle,
+    ContactContactsIndex,
+    ContactContactsSingle
+  },
+  strict: debug
+})
